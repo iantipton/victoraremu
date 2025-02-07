3222bc3f1fc35b8f29507d2e638ae9688416ec74
Ian Tipton <ian@bronzelabs.co.uk>
2025-02-07 10:33:42 +0000

    Initial version of site with placeholder copy.

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,30 +1,40 @@
 // Sidebar Data
 
+const profile = {
+  name: "Victor Aremu",
+  profession: "Android Developer",
+  location: "UK",
+  bio: "Android developer with 8+ years of experience in building high performance apps. Skilled in Java, Kotlin, AndroidX and Firebase.",
+};
+
+const about = {
+  text: "I'm a dedicated Android developer with a passion for building innovative, user-focused solutions using the latest technologies. I thrive on learning new tools and frameworks, and bring a hard-working, detail-oriented approach to every project I tackle.",
+  url: "https://victoraremu.com",
+  url_short: "victoraremu.com",
+  email: "hi@victoraremu.com",
+};
+
 const skills = [
-   "JavaScript",
-   "React",
-   "Node.js",
-   "ExpressJS",
-   "Python",
-   "SQL",
-   "Git",
-   "Agile",
-   "CI/CD",
+  "Kotlin",
+  "Java",
+  "AndroidX",
+  "Jetpack Compose",
+  "Material Design",
+  "Room",
 ];
 
 const details = [
-   { label: "Location", value: "Mountain View, CA" },
-   { label: "Experience", value: ">8 years" },
-   { label: "Availability", value: "In 2 month" },
-   { label: "Relocation", value: "No" },
+  { label: "Location", value: "Newport, UK" },
+  { label: "Experience", value: ">8 years" },
+  { label: "Availability", value: "In 2 month" },
+  { label: "Relocation", value: "No" },
 ];
 
 // Navigation
 
 const navigation = [
-   { name: "Resume", href: "./" },
-   { name: "Projects", href: "./projects.html" },
-   { name: "Blog", href: "./blog.html" },
+  { name: "Resume", href: "./" },
+  { name: "Projects", href: "./projects.html" },
 ];
 
 /**
@@ -34,78 +44,40 @@ const navigation = [
 // Experience Data
 
 const jobsList = [
-   {
-      icon: "./assets/images/google.jpg",
-      role: "Full-time Software Engineer",
-      company: "Google",
-      location: "Mountain View, CA",
-      period: "Jul 2020 - Present",
-      type: "Full-time",
-      description:
-         "Developed and maintained web applications using React, Node.js, and Python. Worked with a team of engineers to deliver high-quality software on time and within budget.",
-   },
-   {
-      icon: "./assets/images/amazon.jpg",
-      role: "Software Engineer Intern",
-      company: "Amazon",
-      location: "Seattle, WA",
-      period: "Feb 2018 - Jun 2020",
-      type: "Full-time",
-      description:
-         "Designed and implemented new features for Amazon's e-commerce platform. Worked on a team of engineers to improve the user experience and increase sales.",
-   },
-   {
-      icon: "./assets/images/microsoft.jpg",
-      role: "Software Development Engineer in Test",
-      company: "Microsoft",
-      location: "Redmond, WA",
-      period: "Aug 2017 - Jan 2018",
-      type: "Full-time",
-      description:
-         "Wrote and executed test cases for Microsoft's Windows operating system. Worked with a team of engineers to ensure the quality of the software.",
-   },
-   {
-      icon: "./assets/images/tesla.jpg",
-      role: "Software Engineer Intern",
-      company: "Tesla",
-      location: "Palo Alto, CA",
-      period: "Dec 2016 - May 2017",
-      type: "Full-time",
-      description:
-         "Developed software for Tesla's autonomous driving vehicles. Worked on a team of engineers to improve the safety and reliability of the software.",
-   },
+  {
+    icon: "./assets/images/bronze-small.jpg",
+    role: "Full-time Software Engineer",
+    company: "Bronze Software Labs",
+    location: "Telford, UK",
+    period: "May 2021 - Present",
+    type: "Lead Developer",
+    description: `<p>Lead developer on the Android version of the Tribe Project. Worked with a team of engineers to deliver a high performance Android app.</p>
+    <br />
+      <p>Also developed and launched the My Telford Android app for Telfod & Wrekin Council that streamlines local issue reporting through intuitive design, secure authentication, and robust data handling, driving efficient community engagement.</p>
+      `,
+  },
+  {
+    icon: "./assets/images/amazon.jpg",
+    role: "[[Another company]]",
+    company: "[[company]]",
+    location: "[[area]]",
+    period: "Feb 2018 - Jun 2020",
+    type: "Full-time",
+    description: "[[Insert description here]]",
+  },
 ];
 
 // Education
 
 const educationList = [
-   {
-      icon: "./assets/images/stanford.jpg",
-      degree: "Master of Science in Computer Science",
-      university: "Stanford University",
-      location: "Stanford, CA",
-      period: "2018 - 2020",
-      description:
-         "Developed a machine learning algorithm to predict customer churn. Built a web application to help students find roommates.",
-   },
-   {
-      icon: "./assets/images/universityofcalifornia.jpg",
-      degree: "Bachelor of Science in Computer Science",
-      university: "University of California",
-      location: "Berkeley, CA",
-      period: "2014 - 2018",
-      description:
-         "Developed a mobile app to help students track their expenses. Built a website for a local business.",
-   },
-   {
-      icon: "./assets/images/deanza.jpg",
-      degree: "Associate of Science in Computer Science",
-      university: "De Anza College",
-      location: "Cupertino, CA",
-      period: "2012 - 2014",
-      description:
-         "Developed a game using Python. Built a website for a student organization.",
-   },
+  {
+    icon: "./assets/images/stanford.jpg",
+    degree: "Master of Science in Computer Science",
+    university: "[[University]]",
+    location: "[[Area]]]",
+    period: "2018 - 2020",
+    description: "[[Description]]",
+  },
 ];
 
 /**
@@ -114,46 +86,38 @@ const educationList = [
 
 // Projects
 const projectsList = [
-   {
-      icon: "./assets/images/codecrafthub.jpg",
-      name: "CodeCraftHub",
-      category: "Developer Tools",
-      role: "Co-Founder",
-      date: "Jun 2021",
-      description:
-         "CodeCraftHub is a collaborative platform for developers, streamlining code review and project management. Enhance your team's productivity with our intuitive tools.",
-      website: "https://codecrafthub.com",
-   },
-   {
-      icon: "./assets/images/ecommerceboost.jpg",
-      name: "E-commerceBoost",
-      category: "E-commerce",
-      role: "Lead Developer",
-      date: "Dec 2022",
-      description:
-         "E-commerceBoost is a full-stack solution for online retailers, offering seamless inventory management, secure payment gateways, and a user-friendly shopping experience.",
-      website: "https://ecommerceboost.io",
-   },
-   {
-      icon: "./assets/images/cloudsnip.jpg",
-      name: "CloudSnip",
-      category: "Cloud Services",
-      role: "Technical Lead",
-      date: "May 2020",
-      description:
-         "CloudSnip is a versatile cloud storage service, providing users with secure file storage, sharing, and synchronization across devices. Experience the next level of cloud convenience.",
-      website: "https://cloudsnip.net",
-   },
-   {
-      icon: "./assets/images/healthtrackr.jpg",
-      name: "HealthTrackr",
-      category: "HealthTech",
-      role: "Founder",
-      date: "Oct 2023",
-      description:
-         "HealthTrackr revolutionizes personal health management, offering a comprehensive platform for tracking fitness, nutrition, and health metrics. Take control of your well-being.",
-      website: "https://healthtrackr.app",
-   },
+  {
+    icon: "./assets/images/tribe-small.jpg",
+    name: "Tribe Project",
+    category: "Android App",
+    role: "Lead Developer",
+    date: "May 2021 - Present",
+    description: `
+    <p>Tribe is a powerful and user-friendly app designed to connect volunteers and support providers with those in need, fostering a strong sense of community. Whether you’re offering your skills or seeking assistance, Tribe makes it easy to find, connect, and manage support with just a few taps.</p>
+      <br />
+      <p>Developing Tribe allowed me to combine my expertise in
+                    Kotlin and GraphQL with a mission-driven project that brings
+                    real value to communities. By leveraging cutting-edge
+                    technology, Tribe provides a seamless and empowering
+                    experience for users looking to give or receive support.</p>      
+    `,
+    website: "tribe.html",
+  },
+  {
+    icon: "./assets/images/my-telford.png",
+    name: "My Telford",
+    category: "Local Government",
+    role: "Lead Developer",
+    date: "Dec 2022",
+    description: `
+       <p>A fast and simple way for local residents to report problems and help keep Telford clean, green, and safe. Whether it’s a pothole, litter, or any other issue, you can get in touch with the right people in just a few taps—no waiting on the phone or waiting for offices to open.</p>
+       <br />
+       <p>
+       Developing My Telford App allowed me to tackle real-world challenges in community reporting and municipal management. By leveraging the above technologies, I was able to deliver an easy-to-use and reliable tool that encourages civic engagement and streamlines problem resolution.
+       </p>
+       `,
+    website: "my-telford.html",
+  },
 ];
 
 /**
@@ -163,28 +127,28 @@ const projectsList = [
 // Posts
 
 const posts = [
-   {
-      date: "February 22, 2024",
-      title: "Building an Online Presence as a Full Stack Developer",
-      excerpt:
-         "Explore the importance of establishing a strong online presence for full stack developers. Learn how to create a compelling portfolio, engage with the developer community, and leverage social media to boost your career.",
-   },
-   {
-      date: "February 15, 2024",
-      title: "Navigating the Full Stack Developer Job Interview Maze",
-      excerpt:
-         "Prepare for success in your full stack developer job interviews. Uncover common interview questions, tips for showcasing your problem-solving skills, and strategies for handling technical assessments.",
-   },
-   {
-      date: "February 8, 2024",
-      title: "Mastering the Art of Full Stack Developer Cover Letters",
-      excerpt:
-         "Explore the art of writing compelling cover letters for full stack developer positions. Learn how to tailor your cover letter to showcase your technical prowess and demonstrate your passion for coding.",
-   },
-   {
-      date: "February 1, 2024",
-      title: "Crafting a Comprehensive Full Stack Developer Resume",
-      excerpt:
-         "Learn the essential elements to include in your full stack developer resume for a stand-out application. Nail down the perfect balance between showcasing skills and work experience.",
-   },
+  {
+    date: "February 22, 2024",
+    title: "Building an Online Presence as a Full Stack Developer",
+    excerpt:
+      "Explore the importance of establishing a strong online presence for full stack developers. Learn how to create a compelling portfolio, engage with the developer community, and leverage social media to boost your career.",
+  },
+  {
+    date: "February 15, 2024",
+    title: "Navigating the Full Stack Developer Job Interview Maze",
+    excerpt:
+      "Prepare for success in your full stack developer job interviews. Uncover common interview questions, tips for showcasing your problem-solving skills, and strategies for handling technical assessments.",
+  },
+  {
+    date: "February 8, 2024",
+    title: "Mastering the Art of Full Stack Developer Cover Letters",
+    excerpt:
+      "Explore the art of writing compelling cover letters for full stack developer positions. Learn how to tailor your cover letter to showcase your technical prowess and demonstrate your passion for coding.",
+  },
+  {
+    date: "February 1, 2024",
+    title: "Crafting a Comprehensive Full Stack Developer Resume",
+    excerpt:
+      "Learn the essential elements to include in your full stack developer resume for a stand-out application. Nail down the perfect balance between showcasing skills and work experience.",
+  },
 ];
